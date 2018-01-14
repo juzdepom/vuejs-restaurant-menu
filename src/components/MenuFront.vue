ec4d37ca8a9e60209b1714e9914511cd0d7f3dfc
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-14 15:33:40 -0800

    Everything working. Separating the different menu sections into separate components

---
+++ src/components/MenuFront.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="rm-cover">
+        <div class="rm-front">
+                <div class="rm-content">
+
+                    <div class="rm-logo"></div>
+                    <h2>Gourmet Castle</h2>
+                    <h3>Fine Dining &amp; Gourmet Takeaway</h3>
+
+                    <a href="#" class="rm-button-open" @click="openMenu">View the Menu</a>
+                    <div class="rm-info">
+                        <p>
+                        <strong>Gourmet Castle Restaurant</strong><br>
+                        246 Wonderful Paradise Ln.<br>
+                        Pasadena, CA 91101<br>
+                        <strong>Phone</strong> 626.511.1170<br>
+                        <strong>Fax</strong> 626.992.1020</p>
+                    </div>
+                </div><!-- /rm-content -->
+        </div><!-- /rm-front -->
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+      openMenu(){
+          var container = $( '#rm-container' )
+          container.addClass( 'rm-open' )
+      }
+  }
+}
+</script>
+
---
+++ src/components/MenuLeft.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="rm-back">
+        <div class="rm-content">
+            <h4>Appetizers</h4>
+            <dl>
+                <dt>Bella's Artichokes</dt>
+                <dd>Roasted artichokes with chipotle aioli and cream cheese</dd>
+
+                <dt>Bruschetta Blue Delight</dt>
+                <dd>Blue cheese and citrus bruschetta</dd>
+
+                <dt>Pomme Dulse</dt>
+                <dd>Baked potatoes with crisped dulse</dd>
+
+                <!-- <dt><a class="rm-viewdetails" data-thumb="../images/1.jpg">Green Love Crostini</a></dt> -->
+                <dt>Green Love Crostini</dt>
+                <dd>Crostini with young pecorino, grilled figs and arugula &amp; mint pesto</dd>
+
+                <dt>Focaccia di Carciofi</dt>
+                <dd>Artichoke focaccia with fresh thyme</dd>
+            </dl>
+
+            <h4>Salads &amp; More</h4>
+            <dl>
+                <dt>Green Delight</dt>
+                <dd>Watercress, frisee and avocado salad</dd>
+
+                <dt><a href="http://herbivoracious.com/2010/02/thai-tofu-salad-recipe.html" class="rm-viewdetails" data-thumb="../images/13.jpg">Gourmet Yam Taohu</a></dt>
+                <dd>Thai tofu salad yam taohu</dd>
+
+                <dt>Panini Deluxe</dt>
+                <dd>Buffalo mozzarella basil panini</dd>
+            </dl>
+        </div><!-- /rm-content -->
+        <div class="rm-overlay"></div>
+    </div><!-- /rm-back -->
+</template>
